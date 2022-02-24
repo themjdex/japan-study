6892b7faf0b061caf9983b9dc634cf098ef8af3b
Marat Zhanpeisov <themjdex@gmail.com>
2022-02-24 11:50:11 +0500

    added back button

--- src/pages/AdvancedPage/AdvancedPage.jsx
+++ src/pages/AdvancedPage/AdvancedPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 const AdvancedPage = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    };
     return (
         <div>
             <h1>popa</h1>
+            <button onClick={goBack}>Назад</button>
         </div>
     );
 };
--- src/pages/BeginnerPage/BeginnerPage.jsx
+++ src/pages/BeginnerPage/BeginnerPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 const BeginnerPage = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    };
+
     return (
         <div>
             <h1>popa</h1>
+            <button onClick={goBack}>Назад</button>
         </div>
     );
 };
